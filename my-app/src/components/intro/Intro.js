fa4fb0b1538fcd8cfdc95d6dfe7d573a7f62bd0b
James Broski <james.broski.5@gmail.com>
2021-12-15 14:34:03 -0700

    introduction progress

--- my-app/src/components/intro/Intro.js
+++ my-app/src/components/intro/Intro.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Intro.css";
 
 const Intro = () => {
-  return <div>this is the introduction, yay!</div>;
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">James Broski</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Frontend</div>
+              <div className="i-title-item">Backend</div>
+              <div className="i-title-item">MERN Stack</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="i-right">right</div>
+    </div>
+  );
 };
 
 export default Intro;
