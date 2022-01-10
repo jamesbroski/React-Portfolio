a5cf5343fd0024efd38f22cdf8105e5eb2fb1e29
James Broski <james.broski.5@gmail.com>
2022-01-10 13:18:10 -0700

    progress on about me section

--- my-app/src/components/about/About.js
+++ my-app/src/components/about/About.js
@@ -1,7 +1,17 @@
 import "./About.css";
-
+import Denver from "../../img/unionstation.png";
 const About = () => {
-  return <div className="a">about</div>;
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img src={Denver} alt="" className="a-img"></img>
+        </div>
+      </div>
+      <div className="a-right">right</div>
+    </div>
+  );
 };
 
 export default About;
