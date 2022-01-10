4dedcfcbb5342709d075530cacee2bd14809fa31
James Broski <james.broski.5@gmail.com>
2022-01-10 12:35:13 -0700

    new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 }
---
+++ my-app/src/components/about/About.js
@@ -0,0 +1,7 @@
+import "./About.css";
+
+const About = () => {
+  return <div className="a">about</div>;
+};
+
+export default About;
