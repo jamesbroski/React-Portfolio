adeaff446bc073392e303174cc27ed2905c1f72b
James Broski <james.broski.5@gmail.com>
2022-01-24 12:45:37 -0700

    first successful dark mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,19 @@ import About from "./components/about/About";
 import ProjectList from "./components/projectList/ProjectList";
 import Contact from "./components/contact/Contact";
 import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
+import { useContext } from "react";
 
-function App() {
+const App = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode ? "white" : "#222",
+      }}
+    >
       <Toggle />
       <Intro />
       <About />
@@ -14,6 +23,6 @@ function App() {
       <Contact />
     </div>
   );
-}
+};
 
 export default App;
