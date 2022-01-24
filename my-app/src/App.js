79b871ed3c5542f5546ae20b239539da2c362ce1
James Broski <james.broski.5@gmail.com>
2022-01-24 11:54:46 -0700

    dark mode component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import ProjectList from "./components/projectList/ProjectList";
 import Contact from "./components/contact/Contact";
+import Toggle from "./components/toggle/Toggle";
 
 function App() {
   return (
     <div>
+      <Toggle />
       <Intro />
       <About />
       <ProjectList />
